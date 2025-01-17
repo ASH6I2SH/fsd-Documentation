1db7bfcc2280ef4cf680cb8a3e094b56abbcfc8b
Ashish Pandey <ash6i2sh@gmail.com>
2025-01-17 20:20:49 +0530

    script.js added to all html files

---
+++ srcipt.js
@@ -0,0 +1,9 @@
+var ans= document.querySelectorAll("h3");
+
+ans.forEach(element => {
+    element.style.color="#e414e8";
+    element.style.margin="1rem";
+    element.style.textDecoration="underline";
+
+    
+});
