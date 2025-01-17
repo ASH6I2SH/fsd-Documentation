24c43a9445c0c8e7e02a1a7ace265b026d676d84
Ashish Pandey <ash6i2sh@gmail.com>
2025-01-18 04:26:23 +0530

    Applied css

--- srcipt.js
+++ srcipt.js
@@ -1,8 +1,32 @@
-var ans= document.querySelectorAll("h3");
+var ans= document.querySelectorAll("h2");
+var para= document.querySelectorAll("p");
+var hand= document.querySelectorAll(".containAnch");
+
+
+
+var count=1;
 
 ans.forEach(element => {
-    element.style.color="#e414e8";
-    element.style.margin="1rem";
-    element.style.textDecoration="underline";
-
+    var classname= element.className;
+    if(classname!="noth2"){
+        var text= element.innerHTML;
+        element.innerHTML= `${count++}. ${text}`;
+    }
+    
 });
+
+para.forEach(element => {
+    var parainnerHtml= element.innerHTML;
+        element.innerHTML= `<h3>${parainnerHtml}</h3>`;
+    
+});
+
+hand.forEach(element => {
+   element.innerHTML+=`<img style="height:1.5rem; width: 1.5rem;" src="../images/hand.png" alt="">`;
+    
+});
+
+
+
+
+
