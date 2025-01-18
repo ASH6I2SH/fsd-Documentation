8176c7cdd4559fc00ef6cc499e820342c39c5ef2
Ashish Pandey <ash6i2sh@gmail.com>
2025-01-18 23:06:55 +0530

    theme changed

--- srcipt.js
+++ srcipt.js
@@ -1,31 +1,38 @@
 var ans= document.querySelectorAll("h2");
 var para= document.querySelectorAll("p");
 var hand= document.querySelectorAll(".containAnch");
-
+var h2hasanch = document.querySelectorAll(".h2hasanch")
+var anch= document.querySelectorAll(".anch")
+ 
 
 
 var count=1;
 
 ans.forEach(element => {
     var classname= element.className;
-    if(classname!="noth2"){
-        var text= element.innerHTML;
-        element.innerHTML= `${count++}. ${text}`;
-    }
+        if((classname != "noth2")){
+            var anstext=element.innerHTML;
+            element.innerHTML=`${count++} ${anstext}`;
+        }
+        
+           
+    });
     
-});
-
-para.forEach(element => {
+    
+   
+    
+    
+    para.forEach(element => {
     var parainnerHtml= element.innerHTML;
         element.innerHTML= `<h3>${parainnerHtml}</h3>`;
     
 });
 
+
 hand.forEach(element => {
    element.innerHTML+=`<img class="insertImg"  src="../images/hand.png" alt="">`;
     
 });
-
 
 
 
