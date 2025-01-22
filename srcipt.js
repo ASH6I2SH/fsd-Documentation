2d4bac94c2f5143137d029943eebd52e31f45f63
Ashish Pandey <ash6i2sh@gmail.com>
2025-01-22 22:51:25 +0530

    content table added

--- srcipt.js
+++ srcipt.js
@@ -3,28 +3,29 @@ var para= document.querySelectorAll("p");
 var hand= document.querySelectorAll(".containAnch");
 var h2hasanch = document.querySelectorAll(".h2hasanch")
 var anch= document.querySelectorAll(".anch")
- 
+var contentList= document.querySelectorAll(".contentList")
 
 
 var count=1;
-
 ans.forEach(element => {
     var classname= element.className;
         if((classname != "noth2")){
             var anstext=element.innerHTML;
             element.innerHTML=`${count++} ${anstext}`;
-        }
-        
-           
+        }  
     });
+
+
+var count2= 1;
+contentList.forEach(((element)=> {
+    var contentListText= element.textContent;
+    element.textContent= `${count2++}. ${contentListText}`
+}))    
+
     
-    
-   
-    
-    
-    para.forEach(element => {
+para.forEach(element => {
     var parainnerHtml= element.innerHTML;
-        element.innerHTML= `<h3>${parainnerHtml}</h3>`;
+    element.innerHTML= `<h3>${parainnerHtml}</h3>`;
     
 });
 
@@ -33,6 +34,26 @@ hand.forEach(element => {
    element.innerHTML+=`<img class="insertImg"  src="../images/hand.png" alt="">`;
     
 });
+
+var lt= gsap.timeline();
+lt.from("h1",{
+    opacity: 0,
+    y: -30,
+    duration: 1,
+    delay:0.5
+})
+lt.from(".contentList",{
+    opacity: 0,
+    y: -30,
+    duration: 1,
+    stagger: 0.2
+})
+
+
+
+
+
+
 
 
 
